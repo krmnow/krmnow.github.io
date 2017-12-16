0bcf7c2a7b31d9a0b22df9c66c3bdde9588b0e39
Krzysiek Nowak <krzysiek.nowak@onet.eu>
2017-12-16 10:35:40 +0100

    zmienne lokalne i globalne

---
+++ warsztaty/js/variable.scope.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var iloscOsoba1 = 3500;
+var iloscOsoba2 = 5000;
+var iloscOsoba3 = 7000;
+var iloscOsoba4 = 2500;
+
+var sumaKasy = iloscOsoba1 + iloscOsoba2 + iloscOsoba3;
+
+console.log('w głównym programie suma kasy wynosi ' + sumaKasy)
+
+function policzSumeKasy(iloscOsoba1, iloscOsoba2, iloscOsoba3){
+    // sumaKasy = 500;
+    var suma = iloscOsoba1 + iloscOsoba2 + iloscOsoba3;
+    console.log('w fukcji suma kasy wynosi ' + sumaKasy);
+        console.log('w fukcji suma wynosi ' + suma);
+
+}
+
+policzSumeKasy(20,30,40)
+
+  console.log('w programie po funkcji suma kasy wynosi ' + sumaKasy);
+
+console.log(suma);
--- warsztaty/js/variables.js
+++ warsztaty/js/variables.js
@@ -8,3 +8,27 @@ name = imiedamskie;
 
 console.log(name);
 console.log(wiek);
+
+function myFunction() {
+    var nazwisko = 'Kowalski';
+    console.log(nazwisko);
+}
+
+myFunction();
+
+var nazwaFunkcji = function () {
+    console.log('funkcja anonimowa');
+}
+nazwaFunkcji();
+
+function mnozenie(parametr1, parametr2, parametr3) {
+    var wynik = parametr1 * parametr2 * parametr3;
+    return wynik;
+}
+console.log(mnozenie(3, 4, 6));
+
+var liczba1 = 5;
+var liczba2 = 13;
+var liczba3 = 8;
+
+console.log(mnozenie(liczba1, liczba2, liczba3));
