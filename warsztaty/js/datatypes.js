bebcc4c0adbe5640eab7d1322c4849cd23e0fa90
Krzysiek Nowak <krzysiek.nowak@onet.eu>
2017-12-16 11:21:51 +0100

    wartosci js

---
+++ warsztaty/js/datatypes.js
@@ -0,0 +1,28 @@
+'use strict'
+/* typ liczbowy */
+var pensja = 5000;
+var premia = 3000;
+var calkowitaKwota = pensja + premia;
+
+console.log(calkowitaKwota);
+
+/* typ łańcuchowy */
+
+var pensjaStr = "5000";
+var premiaStr = "2500";
+
+var calkowitaKwotaStr = pensjaStr + premiaStr;
+console.log(calkowitaKwotaStr);
+
+/* typ logiczny */
+
+var czyJestSmog = true;
+console.log(czyJestSmog);
+
+/* Ty specjalny */
+
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
--- warsztaty/js/variable.scope.js
+++ warsztaty/js/variable.scope.js
@@ -12,13 +12,15 @@ console.log('w głównym programie suma kasy wynosi ' + sumaKasy)
 function policzSumeKasy(iloscOsoba1, iloscOsoba2, iloscOsoba3){
     // sumaKasy = 500;
     var suma = iloscOsoba1 + iloscOsoba2 + iloscOsoba3;
+    
+    var sumaKasy = iloscOsoba1 + iloscOsoba2;
+    
     console.log('w fukcji suma kasy wynosi ' + sumaKasy);
         console.log('w fukcji suma wynosi ' + suma);
+    return suma;
 
 }
 
 policzSumeKasy(20,30,40)
 
   console.log('w programie po funkcji suma kasy wynosi ' + sumaKasy);
-
-console.log(suma);
