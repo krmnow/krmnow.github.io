c0448ea503f0c97aaba5a589b4adc381bf9dd674
Krzysiek Nowak <krzysiek.nowak@onet.eu>
2017-12-16 11:43:12 +0100

    tablice z funkcjami

--- warsztaty/js/tables.js
+++ warsztaty/js/tables.js
@@ -7,4 +7,8 @@ console.log(names[0]);
 names[3] = 'Ola';
 names[2] = 'Kuba';
 
+names.push('Miłosz');
+var ostatniElement = names.pop();
+names.unshift('Robert');
 console.log(names);
+console.log(ostatniElement);
