82381e2195492a121a1aa2edce6d0144c529e535
Krzysiek Nowak <krzysiek.nowak@onet.eu>
2017-12-17 09:52:19 +0100

    js dom cwiczenia

---
+++ warsztaty/js odm/dom2.js
@@ -0,0 +1,8 @@
+var parFirst = document.getElementById('parFirst');
+console.log(parFirst);
+
+var linki = document.getElementsByClassName('superlink');
+console.log(linki);
+
+var linkiPoTagu = document.getElementsByTagName('a');
+console.log(linkiPoTagu);
