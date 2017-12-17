3348801c573cef14ad0f86940ad52baa23f46182
Krzysiek Nowak <krzysiek.nowak@onet.eu>
2017-12-17 10:19:50 +0100

    podstawowe funkcje dom

--- warsztaty/js odm/dom2.js
+++ warsztaty/js odm/dom2.js
@@ -6,3 +6,17 @@ console.log(linki);
 
 var linkiPoTagu = document.getElementsByTagName('a');
 console.log(linkiPoTagu);
+
+var divPoId = document.querySelector('#parSecond');
+console.log(divPoId);
+
+var linkiPoSelektorze = document.querySelectorAll('.link');
+console.log(linkiPoSelektorze);
+
+linkiPoSelektorze.forEach(function (link) {
+    console.log(link);
+
+});
+for (var i = 0; i < linkiPoSelektorze.length; i++) {
+    console.log(linkiPoSelektorze[i]);
+}
