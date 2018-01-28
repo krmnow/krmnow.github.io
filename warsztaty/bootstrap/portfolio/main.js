ec3b5a73155dedcb1a2889baaabdd3d593e4b4f1
Krzysiek Nowak <krzysiek.nowak@onet.eu>
2018-01-28 09:39:20 +0100

    portfolio finansowe

---
+++ warsztaty/bootstrap/portfolio/main.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {   
+    if( $(window).scrollTop() >= 50) {
+$("#main-nav").addClass("scroll");
+    $("#main-nav").css({"transition": "all 0.3s"});
+    }
+    else{
+   $("#main-nav").removeClass("scroll");
+       }
+      
+});
+
