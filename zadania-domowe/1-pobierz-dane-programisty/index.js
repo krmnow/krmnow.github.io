a89b17e196bb5743187d1cd4bf42d0c5042455d1
krmnow <krzysiek.nowak@onet.eu>
2018-02-02 09:10:39 +0100

    Update index.js

--- zadania-domowe/1-pobierz-dane-programisty/index.js
+++ zadania-domowe/1-pobierz-dane-programisty/index.js
@@ -3,10 +3,10 @@ $(function () {
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
             
             $('body').append('<div id="dane-programisty">
-                '<br><div>Imię: ' + data.imie +
-                '<br> Nazwisko: ' + data.nazwisko +
-                '<br> Zawód: ' + data.zawod +
-                '<br> Firma: ' + data.firma
+                <br><div>Imię:  + data.imie +
+                <br> Nazwisko:  + data.nazwisko +
+                <br> Zawód:  + data.zawod +
+                <br> Firma:  + data.firma
                              </div>'
             );
 
