c96f2beb6d4af649689ae000adb9de582024ca7d
Krzysiek Nowak <krzysiek.nowak@onet.eu>
2018-03-07 20:29:42 +0100

    zadania

---
+++ warsztaty/imie.js
@@ -0,0 +1,28 @@
+    "osoby": [
+        {
+            firstName: 'Krystian',
+            lastName: 'Dziopa',
+            age: 180,
+            eyeColor: 'blue',
+
+        },
+
+        {
+            firstName: 'Krystian',
+            lastName: 'Dziopa',
+            age: 180,
+            eyeColor: 'blue',
+        },
+        {
+            firstName: 'Krystian',
+            lastName: 'Dziopa',
+            age: 180,
+            eyeColor: 'blue',
+        }
+    ]
+
+
+
+osoby[1].age.forEach(function (element, index) {
+    console.log(element.age);
+});
